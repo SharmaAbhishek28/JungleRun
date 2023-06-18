549c23d59466985ed6b13655b54f17deb3278aff
SharmaAbhishek28 <135024126+SharmaAbhishek28@users.noreply.github.com>
2023-06-18 23:07:44 +0530

    All animations add

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
         rigidbody = GetComponent<Rigidbody>();
     }
 
+    private float Next_X_POS=0;
+
     // Update is called once per frame
     void Update()
     {
@@ -26,9 +28,34 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("Slide", true);
         }
 
-         else if (Input.GetKeyDown(KeyCode.W))
+        else if (Input.GetKeyDown(KeyCode.W))
         {
             animator.SetBool("Jump", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("Right", true);
+            if(GetComponent<Rigidbody>().position.x >= - 3.5f && GetComponent<Rigidbody>().position.x < -1.5f)
+            {
+                Next_X_POS = 0;
+            }
+            else if(GetComponent<Rigidbody>().position.x >= - 1.5f && GetComponent<Rigidbody>().position.x < 1.5f)
+            {
+                Next_X_POS = 3.5f;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("Left", true);
+
+            if(GetComponent<Rigidbody>().position.x >=  1.5f && GetComponent<Rigidbody>().position.x < 4.5f)
+            {
+                Next_X_POS = 0;
+            }
+            else if(GetComponent<Rigidbody>().position.x >= - 1.5f && GetComponent<Rigidbody>().position.x < 1.5f)
+            {
+                Next_X_POS = -3.5f;
+            }
         }
     }
 
@@ -49,14 +76,30 @@ public class PlayerMovement : MonoBehaviour
     private void OnAnimatorMove()
     {
         if (animator.GetBool("Jump"))
-        {if(isJumpDown)
-            rigidbody.MovePosition(rigidbody.position + new Vector3(0, 0, 2) * animator.deltaPosition.magnitude);
-        else
-            rigidbody.MovePosition(rigidbody.position + new Vector3(0, 1.5f, 2) * animator.deltaPosition.magnitude);    
+        {
+            if(isJumpDown)
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + new Vector3(0, 0, 2) * animator.deltaPosition.magnitude);
+            else
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + new Vector3(0, 1.5f, 2) * animator.deltaPosition.magnitude);    
+        }
+        else if (animator.GetBool("Right"))
+        { 
+            if(GetComponent<Rigidbody>().position.x < Next_X_POS)
+                GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.right * animator.deltaPosition.magnitude);
+            else
+                animator.SetBool("Right", false);
+        }
+
+        else if (animator.GetBool("Left"))
+        { 
+            if(GetComponent<Rigidbody>().position.x > Next_X_POS)
+                GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.left * animator.deltaPosition.magnitude);
+            else
+                animator.SetBool("Left", false);
         }
         else
         {
-            rigidbody.MovePosition(rigidbody.position + Vector3.forward * animator.deltaPosition.magnitude);    
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.forward * animator.deltaPosition.magnitude);    
         }
         
     }
