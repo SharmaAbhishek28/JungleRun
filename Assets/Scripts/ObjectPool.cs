6c01ba02c58713d8ea1287b4b995aae6671bb43b
SharmaAbhishek28 <135024126+SharmaAbhishek28@users.noreply.github.com>
2023-06-19 17:57:16 +0530

    ObjectPool add

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
+    public List<GameObject> PooledObjects;
+    
     // Start is called before the first frame update
     void Start()
     {
