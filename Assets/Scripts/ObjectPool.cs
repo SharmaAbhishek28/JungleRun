512050f77faff3a6e2c994a8ef80b8389e6d912b
SharmaAbhishek28 <135024126+SharmaAbhishek28@users.noreply.github.com>
2023-06-19 18:15:40 +0530

    Shared Instance add

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
+    public static ObjectPool SharedInstace;
     public List<GameObject> PooledObjects;
     public GameObject ObjectToPool;
+    public int AmountToPool;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        Instantiate(ObjectToPool);
+        SharedInstace =  this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
+        PooledObjects = new List<GameObject>();
+        GameObject tmp;
+
+
+
+        for (int i = 0; i < AmountToPool; i++)
+        {
+           tmp = Instantiate(ObjectToPool);    
+           tmp.SetActive(false);
+           PooledObjects.Add(tmp);
+        }
         
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < AmountToPool; i++)
+        {
+            if(!PooledObjects[i].activeInHierarchy)
+            {
+                return PooledObjects[i];
+            }
+        }
+        return null;
     }
 }
