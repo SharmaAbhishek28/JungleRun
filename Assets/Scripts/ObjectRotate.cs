81b8d8a6a6dde90da2321171406c0e2e4f64cced
SharmaAbhishek28 <135024126+SharmaAbhishek28@users.noreply.github.com>
2023-06-22 01:55:17 +0530

    Final Phase Complete

--- Assets/Scripts/ObjectRotate.cs
+++ Assets/Scripts/ObjectRotate.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ObjectRotate : MonoBehaviour
 {
     
     public int rotateSpeed = 1;
+
+  
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,7 @@ public class ObjectRotate : MonoBehaviour
         if(col.gameObject.tag=="player")
         {
             PlayerMovement.score+=1;
+            
             Destroy(gameObject);
         }
     }
